84c0712204a77eb0b830422b9f4c3a17b0c97f97
reVENDRICKal <sa.mubark99@gmail.com>
2025-11-05 17:09:34 +0300

    upgrade on Ai sys

--- Assets/SalmanScripts/Ai.cs
+++ Assets/SalmanScripts/Ai.cs
@@ -1,64 +1,170 @@
-using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Ai : MonoBehaviour
 {
-    [SerializeField]private Transform PlayerPos;
+
+    [SerializeField] GameObject BulletPrefab;
+    enum EnemyStates {Run,attack,Hidden,Look};
+   [SerializeField] EnemyStates States;
     private NavMeshAgent AiEnemy;
     [SerializeField] private Transform[] HiddenPos = new Transform[6];
     [SerializeField] private bool[] HiddenPosIsPlayerIn = new bool[6];
+    [SerializeField] private GameObject Player;
+    RaycastHit Hit;
 
+    float TimeForBullet = 0;
+
+    
+    bool StartThink=true;
 
     void Start()
     {
 
         AiEnemy = GetComponent<NavMeshAgent>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out Hit,4);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) *50, Color.red); 
 
-        if (AiEnemy.velocity == Vector3.zero)
+
+        switch (States)
         {
-            
-        AiEnemy.SetDestination(ChooseBestHiddenPos());
+
+            case EnemyStates.Run:
+
+                if (AiEnemy.velocity == Vector3.zero )
+                                    
+                        {
+                                
+                            AiEnemy.SetDestination(ChooseBestHiddenPos());
+                            States = EnemyStates.Hidden;
+
+                                        
+                        }
+
+
+
+
+                break;
+
+            case EnemyStates.attack:
+
+                // Debug.Log(TimeForBullet);
+                transform.LookAt(Player.transform);
+
+               TimeForBullet += Time.deltaTime;
+                if (TimeForBullet > 1)
+                {
+
+                            TimeForBullet = 0;
+                            Instantiate(BulletPrefab, gameObject.transform.position,gameObject.transform.rotation);
+                        }
+
+
+                break;
+
+
+            case EnemyStates.Hidden:
+
+                if (AiEnemy.velocity == Vector3.zero)
+                {
+                    States = EnemyStates.attack;
+                }
+
+
+
+
+
+                break;
+
+
+            case EnemyStates.Look:
+
+
+
+
+                break;
 
 
 
         }
+
+
+        
     }
 
     Vector3 ChooseBestHiddenPos()
     {
-        float ShorterDis=100; 
-        float recentDis=100;
-        Transform ShortestPath = null;
-        int shorterIndex=0;
-                // Debug.Log(ShorterDis);
+        //dis from ai to hidden pos
+        float ShorterDis = 100;
+        float recentDis = 100;
 
+        //longest pos from player to hidden pos
+        float LongestDisFromPlayerToPos = 0;
+        float recentDisFromPlayerToHiddenPos = 0;
+
+
+
+        Transform PlayerPos = Player.transform;
+        int shorterIndex = 8;
+
+        
 
         for (int i = 0; i < HiddenPos.Length; i++)
         {
-           
-recentDis = Vector3.Distance(transform.position, HiddenPos[i].position);
 
-
-            if (recentDis < ShorterDis && HiddenPosIsPlayerIn[i] == false)
+            if (HiddenPosIsPlayerIn[i] == false)
             {
-                ShorterDis = recentDis;
-                shorterIndex = i;
+                recentDis = Vector3.Distance(transform.position, HiddenPos[i].position);
+                recentDisFromPlayerToHiddenPos = Vector3.Distance(PlayerPos.position, HiddenPos[i].position);
+
+                if (recentDis < ShorterDis   )
+                {
+
+                    if (recentDisFromPlayerToHiddenPos > LongestDisFromPlayerToPos) { 
+                    LongestDisFromPlayerToPos = recentDisFromPlayerToHiddenPos;
+                    ShorterDis = recentDis;
+                    shorterIndex = i;
+                    }
+                }
             }
 
 
 
-
         }
-         HiddenPosIsPlayerIn[shorterIndex] = true;
+
+
+
+        resetTheIsAiHiddenBool(shorterIndex);
+
         return HiddenPos[shorterIndex].position;
 
+
+
+    }
+    void makeBoolTrue()
+    {
+        StartThink = true;
+
+    }
+
+
+    void resetTheIsAiHiddenBool(int j)
+    {
+        for (int i = 0; i < HiddenPosIsPlayerIn.Length; i++)
+        {
+
+            HiddenPosIsPlayerIn[i] = false;
+
+
+        }
+
+        HiddenPosIsPlayerIn[j] = true;
 
 
     }
