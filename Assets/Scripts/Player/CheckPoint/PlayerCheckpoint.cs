5b66b3ff274a83506bcb2f3ad2398cde55d1708b
AshenFares <killerfares@gmail.com>
2025-11-20 19:24:28 +0300

    Push

--- Assets/Scripts/Player/CheckPoint/PlayerCheckpoint.cs
+++ Assets/Scripts/Player/CheckPoint/PlayerCheckpoint.cs
@@ -6,7 +6,12 @@ public class PlayerCheckpoint : MonoBehaviour
     public Transform[] checkpoints;     // Assign in inspector
     private Transform lastCheckpoint;   // The most recent checkpoint
     public MMF_Player respawnFeedback;
-
+    public GameObject HP1;
+    public GameObject HP2;
+    public GameObject HP3;
+    int lives = 3;
+    
+bool enter1Time = false;
     void Start()
     {
         // Default to first checkpoint at start
@@ -17,9 +22,30 @@ public class PlayerCheckpoint : MonoBehaviour
     void Update()
     {
         // Check if player has fallen below Y = -20
-        if (transform.position.y < -10f)
+        if (transform.position.y < -10f && transform.position.y> -10.50f)
         {
             Respawn();
+            
+            if (lives > 0)
+            {
+                lives--;
+                if (lives == 2)
+                {
+                    HP3.SetActive(false);
+                    return;
+                }
+                else if (lives == 1)
+                {
+                    HP2.SetActive(false);
+                     return;
+                }
+                else if (lives == 0)
+                {
+                    HP1.SetActive(false);
+                     return;
+                    // Handle game over logic here if needed
+                }
+            }
         }
     }
 
