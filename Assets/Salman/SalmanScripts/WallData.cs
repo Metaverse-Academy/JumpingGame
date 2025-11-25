a954492bd46fdd7e72771e35d6f95c84c9e075c4
reVENDRICKal <sa.mubark99@gmail.com>
2025-11-25 20:59:33 +0300

    Final Map1

--- Assets/Salman/SalmanScripts/WallData.cs
+++ Assets/Salman/SalmanScripts/WallData.cs
@@ -5,14 +5,12 @@ public class WallData : MonoBehaviour
 
 
     [SerializeField] public bool IsFinalWall;
-      float amplitude = 0.6f, duration = 2f;
+     [SerializeField] float amplitude = 0.6f, duration = 2f;
     void Start()
     {
         LeanTween.moveY(gameObject, transform.position.y + amplitude, duration)
                  .setLoopPingPong()
                  .setEaseInOutSine();
-       LeanTween.moveX(gameObject, transform.position.x + amplitude, duration)
-                 .setLoopPingPong()
-                 .setEaseInOutSine();          
+              
 }
 }
